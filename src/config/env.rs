b19144963bfbb0a4d3343455814d49cd8c68946e
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-04-16 23:54:58 +0530

    feat(crate): configured cloudflare r2

--- src/config/env.rs
+++ src/config/env.rs
@@ -53,6 +53,26 @@ pub struct Env {
     #[serde(deserialize_with = "util::deserialize_arc_str")]
     pub turnstile_site_secret: Arc<str>,
 
+    #[validate(length(min = 1, message = "must not be empty"))]
+    #[serde(deserialize_with = "util::deserialize_arc_str")]
+    pub cloudinary_token_value: Arc<str>,
+
+    #[validate(length(min = 1, message = "must not be empty"))]
+    #[serde(deserialize_with = "util::deserialize_arc_str")]
+    pub cloudinary_access_key_id: Arc<str>,
+
+    #[validate(length(min = 1, message = "must not be empty"))]
+    #[serde(deserialize_with = "util::deserialize_arc_str")]
+    pub cloudinary_access_key_secret: Arc<str>,
+
+    #[validate(length(min = 1, message = "must not be empty"))]
+    #[serde(deserialize_with = "util::deserialize_arc_str")]
+    pub cloudinary_endpoint: Arc<str>,
+
+    #[validate(length(min = 1, message = "must not be empty"))]
+    #[serde(deserialize_with = "util::deserialize_arc_str")]
+    pub cloudinary_bucket_name: Arc<str>,
+
     #[validate(range(min = 8080, max = 8090, message = "must be between 8080 and 8090"))]
     pub port: u16,
 }
