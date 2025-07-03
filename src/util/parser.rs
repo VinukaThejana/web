1938ae38aa500f5b91d28586ae5150bdcd078557
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-07-03 14:59:03 +0530

    fix(performace): increase the parsing performance

--- src/util/parser.rs
+++ src/util/parser.rs
@@ -1,5 +1,12 @@
+use lol_html::{HtmlRewriter, Settings, element, text};
 use pulldown_cmark::{Options, Parser, html};
-use scraper::{Html, Selector};
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Default)]
+struct TableState {
+    headers: Vec<String>,
+    current_column_index: usize,
+}
 
 pub fn cmark(input: &str) -> String {
     let mut options = Options::empty();
@@ -13,91 +20,73 @@ pub fn cmark(input: &str) -> String {
     options.insert(Options::ENABLE_WIKILINKS);
 
     let parser = Parser::new_ext(input, options);
-
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
 
-    let document = Html::parse_document(&html_output);
-    let table_selector = Selector::parse("table").unwrap();
-    let tables: Vec<_> = document.select(&table_selector).collect();
-    if tables.is_empty() {
-        return html_output;
-    }
+    let state = Rc::new(RefCell::new(TableState::default()));
+    let mut output = Vec::new();
 
-    let mut modified_html = html_output;
-
-    for table_element in tables {
-        let th_selector = Selector::parse("thead > tr > th").unwrap();
-        let headers: Vec<String> = table_element
-            .select(&th_selector)
-            .map(|th| th.text().collect::<String>())
-            .collect();
-        if headers.is_empty() {
-            continue;
-        }
-
-        let mut new_table_str = String::new();
-
-        new_table_str.push_str("<table");
-        for (name, value) in table_element.value().attrs() {
-            new_table_str.push_str(&format!(" {}=\"{}\"", name, value.replace('"', "&quot;")));
-        }
-        new_table_str.push('>');
-
-        // Re-add the thead as it is
-        let thead_selector = Selector::parse("thead").unwrap();
-        if let Some(thead) = table_element.select(&thead_selector).next() {
-            new_table_str.push_str(&thead.html());
-        }
-
-        let tbody_selector = Selector::parse("tbody").unwrap();
-        let tr_selector = Selector::parse("tr").unwrap();
-        let td_selector = Selector::parse("td").unwrap();
-
-        if let Some(tbody) = table_element.select(&tbody_selector).next() {
-            new_table_str.push_str("<tbody>");
-            for tr in tbody.select(&tr_selector) {
-                new_table_str.push_str("<tr");
-                for (name, value) in tr.value().attrs() {
-                    new_table_str.push_str(&format!(
-                        " {}=\"{}\"",
-                        name,
-                        value.replace('"', "&quot;")
-                    ));
-                }
-                new_table_str.push('>');
-
-                for (i, td) in tr.select(&td_selector).enumerate() {
-                    let header_text = headers.get(i).map_or("", |h| h.as_str());
-
-                    new_table_str.push_str("<td data-label=\"");
-                    new_table_str.push_str(&header_text.replace('"', "&quot;"));
-                    new_table_str.push('"');
-                    // Avoid duplicating the data-label if somehow it is already added
-                    for (name, value) in td.value().attrs() {
-                        if name.to_lowercase() != "data-label" {
-                            new_table_str.push_str(&format!(
-                                " {}=\"{}\"",
-                                name,
-                                value.replace('"', "&quot;")
-                            ));
-                        }
+    let mut rewriter = HtmlRewriter::new(
+        Settings {
+            element_content_handlers: vec![
+                // Open links in a new tab if they are external links
+                element!("a[href]", |el| {
+                    let href = match el.get_attribute("href") {
+                        Some(href) => href,
+                        None => return Ok(()),
+                    };
+                    if href.starts_with("http://")
+                        || href.starts_with("https://")
+                        || href.starts_with("//")
+                    {
+                        el.set_attribute("target", "_blank")?;
+                        el.set_attribute("rel", "noopener noreferrer")?;
                     }
-                    new_table_str.push('>');
-                    new_table_str.push_str(&td.inner_html());
-                    new_table_str.push_str("</td>");
-                }
+                    Ok(())
+                }),
+                // Add data attributes to tables
+                // Reset table state when a new table starts
+                element!("table", |_| {
+                    let mut state = state.borrow_mut();
+                    state.headers.clear();
+                    state.current_column_index = 0;
+                    Ok(())
+                }),
+                // Capture the text content of each header cell `<th>`.
+                // We must handle cases where text comes in multiple chunks.
+                element!("thead > tr > th", |_| {
+                    state.borrow_mut().headers.push(String::new());
+                    Ok(())
+                }),
+                text!("thead > tr > th", |chunk| {
+                    if let Some(last_header) = state.borrow_mut().headers.last_mut() {
+                        last_header.push_str(chunk.as_str());
+                    }
+                    Ok(())
+                }),
+                element!("tbody > tr", |_| {
+                    state.borrow_mut().current_column_index = 0;
+                    Ok(())
+                }),
+                element!("tbody > tr > td", |el| {
+                    let mut state = state.borrow_mut();
+                    let header_txt = state
+                        .headers
+                        .get(state.current_column_index)
+                        .cloned()
+                        .unwrap_or_default();
+                    el.set_attribute("data-label", &header_txt)?;
+                    state.current_column_index += 1;
+                    Ok(())
+                }),
+            ],
+            ..Settings::default()
+        },
+        |c: &[u8]| output.extend_from_slice(c),
+    );
 
-                new_table_str.push_str("</tr>");
-            }
-            new_table_str.push_str("</tbody>");
-        }
+    rewriter.write(html_output.as_bytes()).unwrap();
+    rewriter.end().unwrap();
 
-        new_table_str.push_str("</table>");
-
-        let original_table_html = table_element.html();
-        modified_html = modified_html.replace(&original_table_html, &new_table_str);
-    }
-
-    modified_html
+    String::from_utf8(output).unwrap()
 }
